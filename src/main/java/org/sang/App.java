0d591dbeca5de7c4fd13394d63769c4ae7425026
lizhibin <1030379170@qq.com>
2021-10-13 17:46:51 +0800

    【新增】入口类注解、banner定制 、Web容器配置、Properties配置和YAML配置等

--- src/main/java/org/sang/App.java
+++ src/main/java/org/sang/App.java
@@ -1,13 +1,19 @@
 package org.sang;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
 
-@EnableAutoConfiguration
-@ComponentScan
+//@EnableAutoConfiguration
+//@ComponentScan
+@SpringBootApplication
 public class App {
     public static void main(String[] args){
-        SpringApplication.run(App.class,args);
+//        SpringApplication.run(App.class,args);
+        SpringApplicationBuilder builder = new SpringApplicationBuilder(App.class);
+        builder.bannerMode(Banner.Mode.CONSOLE).run(args);
     }
 }
---
+++ src/main/java/org/sang/BookController.java
@@ -0,0 +1,15 @@
+package org.sang;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BookController {
+    @Autowired
+    private Book book;
+    @GetMapping("/book")
+    public String book(){
+        return book.getName();
+    }
+}

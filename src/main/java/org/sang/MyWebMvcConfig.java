81f9a99b7c47eec5dda34fbe60fa77b2538272db
lizhibin <1030379170@qq.com>
2021-10-19 11:08:33 +0800

    【新增】解决跨域

--- src/main/java/org/sang/MyWebMvcConfig.java
+++ src/main/java/org/sang/MyWebMvcConfig.java
@@ -1,6 +1,7 @@
 package org.sang;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -19,5 +20,14 @@ public class MyWebMvcConfig implements WebMvcConfigurer {
         registry
                 .addResourceHandler("/static/**")
                 .addResourceLocations("classpath:/static/");
+    }
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")// 允许跨域访问的路径
+                .allowedOrigins("*")// 允许跨域访问的源
+                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")// 允许请求方法
+                .maxAge(168000)// 预检间隔时间
+                .allowedHeaders("*")// 允许头部设置
+                .allowCredentials(true);  // 是否发送cookie
     }
 }
